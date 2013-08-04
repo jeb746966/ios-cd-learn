a9f9e3143d7642d62482b98c259bc800b3d9987f
John Bell <jeb746966@yahoo.co.uk>
2013-08-04 13:01:08 +0100

    latest

--- FailedBankCD/FBCDMasterViewController.h
+++ FailedBankCD/FBCDMasterViewController.h
@@ -8,11 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-#import <CoreData/CoreData.h>
-
-@interface FBCDMasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
-
-@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
-@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-
+@interface FBCDMasterViewController : UITableViewController
+@property (nonatomic,strong) NSManagedObjectContext* managedObjectContext;
 @end
